2f9df26ed2deaf1a28f0ada50a1c5cd073665ba6
Kelthon <kelthonbalbino@gmail.com>
2024-05-15 15:43:06 -0300

    refact!: created vite react-ts project, added prettier and remixicon for react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Message from './components/Message/Message';
+
+function App() {
+  return (
+    <>
+      <Message
+        title="&#x1F6A7; Under Construction &#x1F6A7;"
+        message="I'm currently working hard to improve your browsing experience. During
+          this phase, you may encounter some glitches or errors. I apologize for
+          any inconvenience and thank you for your patience. Thanks for the
+          visit! Thank you for visiting!"
+      />
+    </>
+  );
+}
+
+export default App
