91cd6c9f5b0ce7bb198ba2714c975d83b5ae7530
Kelthon <kelthonbalbino@gmail.com>
2024-06-20 18:30:09 -0300

    feat: created navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import { RiEarthLine, RiSunLine } from '@remixicon/react';
+import './Navbar.css';
+
+function Navbar(): JSX.Element {
+  return (
+    <nav id="navbar">
+      <ul className="nb-items">
+        <li className="nb-item">
+          <ul className="nb-group">
+            <li className="nb-g-item nb-selected">
+              <a className="nb-link" href="#about">
+                about me
+              </a>
+            </li>
+            <li className="nb-g-item">
+              <a className="nb-link" href="#projects">
+                projects
+              </a>
+            </li>
+            <li className="nb-g-item">
+              <a className="nb-link" href="#contact">
+                contact
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="nb-item">
+          <ul className="nb-group">
+            <li className="nb-g-item">
+              <button type="button" aria-label="change color theme">
+                <RiSunLine />
+              </button>
+            </li>
+            <li className="nb-g-item">
+              <RiEarthLine />
+              <select
+                name="page-language"
+                id="nb-change-language"
+                aria-label="change page language"
+              >
+                <option value="en">en</option>
+                <option value="pt-br">pt-br</option>
+              </select>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
