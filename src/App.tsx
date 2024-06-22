0017300b5e3aa4752d1c9956d1764edf5c77cb0b
Kelthon <kelthonbalbino@gmail.com>
2024-06-22 20:55:10 -0300

    feat: added styles to app component, added main and aside tags, added AboutMe section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import AboutMeSection from './components/AboutMe/AboutMe';
 import Message from './components/Message/Message';
 import Navbar from './components/Navbar/Navbar';
 import { ThemeProvider } from './hooks/theme';
@@ -6,14 +7,19 @@ import { ThemeProvider } from './hooks/theme';
 function App() {
   return (
     <ThemeProvider>
-      <Message
-        title="&#x1F6A7; Under Construction &#x1F6A7;"
-        message="I'm currently working hard to improve your browsing experience. During
+      <main>
+        <aside>
+          <Message
+            title="&#x1F6A7; Under Construction &#x1F6A7;"
+            message="I'm currently working hard to improve your browsing experience. During
           this phase, you may encounter some glitches or errors. I apologize for
           any inconvenience and thank you for your patience. Thanks for the
           visit! Thank you for visiting!"
-      />
-      <Navbar />
+          />
+        </aside>
+        <Navbar />
+        <AboutMeSection />
+      </main>
     </ThemeProvider>
   );
 }
