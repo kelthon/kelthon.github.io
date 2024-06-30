350d16e4b17c18d0fc0e404c6f28ea7aad6db6e2
Kelthon <kelthonbalbino@gmail.com>
2024-06-30 18:37:04 -0300

    feat: added container to wrap Navbar and About me section, integrated data module

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,19 @@ import AboutMeSection from './components/AboutMe/AboutMe';
 import Message from './components/Message/Message';
 import Navbar from './components/Navbar/Navbar';
 import { ThemeProvider } from './hooks/theme';
+import { messages } from './data/data';
 
 function App() {
   return (
     <ThemeProvider>
       <main>
-        <aside>
-          <Message
-            title="&#x1F6A7; Under Construction &#x1F6A7;"
-            message="I'm currently working hard to improve your browsing experience. During
-          this phase, you may encounter some glitches or errors. I apologize for
-          any inconvenience and thank you for your patience. Thanks for the
-          visit! Thank you for visiting!"
-          />
-        </aside>
-        <Navbar />
-        <AboutMeSection />
+        <div className="container">
+          <aside>
+            <Message title={messages[0].title} message={messages[0].message!} />
+          </aside>
+          <Navbar />
+          <AboutMeSection />
+        </div>
       </main>
     </ThemeProvider>
   );
