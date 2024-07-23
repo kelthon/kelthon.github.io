731c651e4beda385cb24698e24a87ea94ab678d3
Kelthon <kelthonbalbino@gmail.com>
2024-07-23 13:31:09 -0300

    feat: added new relative imports

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,21 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': `${path.resolve(__dirname, './src')}`,
-      '@components': `${path.resolve(__dirname, './src/components')}`,
       '@config': `${path.resolve(__dirname, './src/config')}`,
-      '@constants': `${path.resolve(__dirname, './src/constants')}`,
+      '@components': `${path.resolve(__dirname, './src/components')}`,
       '@hooks': `${path.resolve(__dirname, './src/hooks')}`,
       '@pages': `${path.resolve(__dirname, './src/pages')}`,
-      '@styles': `${path.resolve(__dirname, './src/styles')}`,
       '@services': `${path.resolve(__dirname, './src/services')}`,
+      '@styles': `${path.resolve(__dirname, './src/styles')}`,
       '@types': `${path.resolve(__dirname, './src/types')}`,
       '@utils': `${path.resolve(__dirname, './src/utils')}`,
+
+      /* Components */
+      '@buttons': `${path.resolve(__dirname, './src/components/buttons')}`,
+      '@cards': `${path.resolve(__dirname, './src/components/cards')}`,
+      '@images': `${path.resolve(__dirname, './src/components/images')}`,
+      '@navs': `${path.resolve(__dirname, './src/components/navs')}`,
+      '@selects': `${path.resolve(__dirname, './src/components/selects')}`,
     },
   },
 });
