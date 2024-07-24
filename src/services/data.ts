c83c5af23cb19a777af2d1ae94275751f3e51b0d
Kelthon <kelthonbalbino@gmail.com>
2024-07-24 17:29:35 -0300

    feat: updated Ptoject type, added descriptions to projects

--- src/services/data.ts
+++ src/services/data.ts
@@ -1,15 +1,41 @@
 export type Project = {
   id: number;
   title: string;
-  description?: string;
-  url?: string;
+  description: string;
+  url: string;
 };
 
 export const projects: Project[] = [
-  { id: 1, title: 'Ecommerce UI' },
-  { id: 2, title: 'Furniture Ecommerce' },
-  { id: 3, title: 'DSV' },
-  { id: 4, title: 'Web chat UI' },
+  {
+    id: 1,
+    title: 'Ecommerce UI',
+    description: 'A landing page of a ecommerce inspired a public template',
+    url: 'https://github.com/kelthon/ecommerce-ui',
+  },
+  {
+    id: 2,
+    title: 'Furniture Ecommerce (API)',
+    description: 'A complete back end for a ecommerce app',
+    url: 'https://github.com/kelthon/ecommerce-api',
+  },
+  {
+    id: 3,
+    title: 'Furniture Ecommerce (APP)',
+    description: 'A complete front end for a ecommerce app',
+    url: 'https://github.com/kelthon/ecommerce-app',
+  },
+  {
+    id: 4,
+    title: 'Data Structure Viewer',
+    description: 'A app to analise, build and view data structures',
+    url: 'https://github.com/kelthon/dsv',
+  },
+  {
+    id: 5,
+    title: 'Web chat UI',
+    description: 'A landing page of a webchat inspired on WhatsApp',
+    url: 'https://github.com/kelthon/webchat',
+  },
 ];
 
 export type Message = {
