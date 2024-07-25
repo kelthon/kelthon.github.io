0e2bf53cdae496ffc62564c352cd050799ef4eee
Kelthon <kelthonbalbino@gmail.com>
2024-07-25 15:47:37 -0300

    fix: class name of element p on project description was fixed

--- src/pages/MyProjects/MyProjects.tsx
+++ src/pages/MyProjects/MyProjects.tsx
@@ -8,7 +8,7 @@ function MyProjects() {
         <span className="pl-pi-counter">{index + 1}.</span>
         <a className="pl-pi-link" href={project.url}>
           <h3 className="pl-pi-lnk-title">{project.title}</h3>
-          <p className="pl-pi-lnk-url">
+          <p className="pl-pi-lnk-description">
             <small>{project.description}</small>
           </p>
         </a>
