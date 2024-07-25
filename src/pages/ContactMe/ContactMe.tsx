48a34327dbda675907e3401926f50103e33758ca
Kelthon <kelthonbalbino@gmail.com>
2024-07-25 19:21:05 -0300

    feat: added link icons, improved hover animatios

--- src/pages/ContactMe/ContactMe.tsx
+++ src/pages/ContactMe/ContactMe.tsx
@@ -1,4 +1,9 @@
-import { RiGithubFill, RiLinkedinBoxFill, RiMailFill } from '@remixicon/react';
+import {
+  RiExternalLinkFill,
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiMailFill,
+} from '@remixicon/react';
 import './ContactMe.css';
 
 function ContactMe(): JSX.Element {
@@ -13,18 +18,23 @@ function ContactMe(): JSX.Element {
         <div className="contact-list">
           <p className="wrapper">
             <RiGithubFill />
-            <a href="https://github.com/kelthon">github.com/kelthon</a>
+            <a href="https://github.com/kelthon">
+              github.com/kelthon
+              <RiExternalLinkFill />
+            </a>
           </p>
           <p className="wrapper">
             <RiLinkedinBoxFill />
-            <a href="https://linkedin.com/kelthon-alves">
+            <a href="https://linkedin.com/in/kelthon-alves/">
               linkedin.com/kelthon-alves
+              <RiExternalLinkFill />
             </a>
           </p>
           <p className="wrapper">
             <RiMailFill />
             <a href="mailto:kelthonbalbino@gmail.com">
               kelthonbalbino@gmail.com
+              <RiExternalLinkFill />
             </a>
           </p>
         </div>
--- src/pages/MyProjects/MyProjects.tsx
+++ src/pages/MyProjects/MyProjects.tsx
@@ -1,15 +1,27 @@
 import { projects } from '@services/data';
 import './MyProjects.css';
+import { RiExternalLinkFill } from '@remixicon/react';
 
 function MyProjects() {
   const projectList = projects.map((project, index) => {
     return (
       <li className="pl-project-item" key={project.id}>
-        <span className="pl-pi-counter">{index + 1}.</span>
+        <p className="pl-pi-counter">
+          <span>{index + 1}</span>
+          <span>.</span>
+        </p>
         <a className="pl-pi-link" href={project.url}>
-          <h3 className="pl-pi-lnk-title">{project.title}</h3>
+          <h3 className="pl-pi-lnk-title">
+            <span>{project.title}</span>
+            <span>
+              <RiExternalLinkFill />
+            </span>
+          </h3>
           <small className="pl-pi-lnk-description">{project.description}</small>
         </a>
+        <span>
+          <RiExternalLinkFill />
+        </span>
       </li>
     );
   });
